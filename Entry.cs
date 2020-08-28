e6fccf3c83288f441345b0088ed6abf3b35fd0eb
llxzy <snowcrash@tuta.io>
2020-08-28 17:29:58 +0200

    Entry base, date tbd later due to db

---
+++ Entry.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using athene.Enums;
+using GLib;
+
+namespace athene
+{
+    public class Entry
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public EntryFormat Format { get; set; }
+        public EntryStatus Status { get; set; }
+        public int Score { get; set; }
+        //public DateTime Added { get; set; }
+
+        public Entry(string title, string author)
+        {
+            Title = title;
+            Author = author;
+            Score = 0;
+            //Added = DateTime.NewNowUtc();
+        }
+    }
+}
