f087d60209e7c032801685016cacdf8790beb94a
llxzy <snowcrash@tuta.io>
2020-09-02 22:29:19 +0200

    cleaned up

--- Program.cs
+++ Program.cs
@@ -15,29 +15,14 @@ namespace athene
             var app = new Application("org.athene.athene", GLib.ApplicationFlags.None);
             app.Register(GLib.Cancellable.Current);
 
-            /*using (var db = new DatabaseContext())
-            {
-                db.Entries.Add(new Entry("e", "x"));
-                db.SaveChanges();
-            }*/
             //ClearDB();
             //TODO Scrolling in treeview
             
-            //var win = new MainWindow();
             var win = new ApplicationMainWindow();
-            //var win = new AddEntryWindow();
             app.AddWindow(win);
 
             win.Show();
             Application.Run();
-            /*var entry = new Entry("aaa", "testauthor");
-            Console.WriteLine($"{entry.Title} by {entry.Author}");
-            using (var ctx = new DatabaseContext())
-            {
-                Console.WriteLine(ctx.Entries.Count());
-                ctx.Entries.Add(entry);
-                ctx.SaveChanges();
-            }*/
         }
 
         private static void ClearDB()
