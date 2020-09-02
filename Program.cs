d745025eab5c42c114b64bb1d0a4bb120166565f
llxzy <snowcrash@tuta.io>
2020-09-02 22:10:32 +0200

    This is a mess but needs to be updated to reflect how it works

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using athene.Resources;
 using Gtk;
 
 namespace athene
@@ -13,11 +15,41 @@ namespace athene
             var app = new Application("org.athene.athene", GLib.ApplicationFlags.None);
             app.Register(GLib.Cancellable.Current);
 
-            var win = new MainWindow();
+            /*using (var db = new DatabaseContext())
+            {
+                db.Entries.Add(new Entry("e", "x"));
+                db.SaveChanges();
+            }*/
+            //ClearDB();
+            //TODO Scrolling in treeview
+            
+            //var win = new MainWindow();
+            var win = new ApplicationMainWindow();
+            //var win = new AddEntryWindow();
             app.AddWindow(win);
 
             win.Show();
             Application.Run();
+            /*var entry = new Entry("aaa", "testauthor");
+            Console.WriteLine($"{entry.Title} by {entry.Author}");
+            using (var ctx = new DatabaseContext())
+            {
+                Console.WriteLine(ctx.Entries.Count());
+                ctx.Entries.Add(entry);
+                ctx.SaveChanges();
+            }*/
+        }
+
+        private static void ClearDB()
+        {
+            using (var db = new DatabaseContext())
+            {
+                foreach (var e in db.Entries)
+                {
+                    db.Entries.Remove(e);
+                }
+                db.SaveChanges();
+            }
         }
     }
 }
